acbf365151d3f3e41e11efaac9c1414d405cc1f0
geekyraghav <49072935+geekyraghav@users.noreply.github.com>
2026-01-13 22:30:11 +0530

    Add files via upload

--- DP/NthFibonacciNumber.java
+++ DP/NthFibonacciNumber.java
@@ -15,13 +15,26 @@ public class NthFibonacciNumber {
 //        return fibo(n,dp);
 //    }
 
-    public int nthFibonacci(int n) { // tabulation
-        int[] dp = new int[n+1]; // 0 to n index
+//    public int nthFibonacci(int n) { // tabulation
+//        int[] dp = new int[n+1]; // 0 to n index
+//        dp[0] = 0;
+//        if(n>=1) dp[1] = 1;
+//        for(int i=2;i<=n;i++){
+//            dp[i] = dp[i-1] + dp[i-2];
+//        }
+//        return dp[n];
+//    }
+
+    public int nthFibonacci(int n) { // space optimised tabulation
+        if(n<=1) return n;
+        int[] dp = new int[3]; // 0 to n index
         dp[0] = 0;
-        if(n>=1) dp[1] = 1;
+        dp[1] = 1;
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            dp[2] = dp[1] + dp[0];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
         }
-        return dp[n];
+        return dp[2];
     }
 }
--- DP/SticklerThief.java
+++ DP/SticklerThief.java
@@ -3,17 +3,44 @@ package DP;
 import java.util.Arrays;
 
 public class SticklerThief {
-    public int findMaxSum(int arr[]) {
-        int[] dp = new int[arr.length]; // i : 0 to n-1
-        Arrays.fill(dp,-1);
-        return loot(arr,0,dp);
+    public int findMaxSum(int arr[]) { // space optimised tabulation
+        int n = arr.length;
+        int[] dp = new int[3]; // i : 0 to n-1
+        dp[0] = arr[0];
+        if(n>1) dp[1] = Math.max(arr[0],arr[1]);
+        if(n<=2) return dp[n-1];
+        for(int i=2;i<n;i++){
+            dp[2] = Math.max(arr[i] + dp[0],dp[1]);
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+        return dp[2];
     }
 
-    private int loot(int[] arr, int i, int[] dp) {
-        if(i>=arr.length) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int pick = arr[i] + loot(arr,i+2,dp);
-        int skip = loot(arr,i+1,dp);
-        return dp[i] = Math.max(pick,skip); // dp[i] stores loot(i)
-    }
+//    public int findMaxSum(int arr[]) { // tabulation
+//        int n = arr.length;
+//        int[] dp = new int[n]; // i : 0 to n-1
+//        dp[0] = arr[0];
+//        if(n>1) dp[1] = Math.max(arr[0],arr[1]);
+//        for(int i=2;i<n;i++){
+//            int pick = arr[i] + dp[i-2];
+//            int skip = dp[i-1];
+//            dp[i] = Math.max(pick,skip);
+//        }
+//        return dp[n-1];
+//    }
+
+//    public int findMaxSum(int arr[]) {
+//        int[] dp = new int[arr.length]; // i : 0 to n-1
+//        Arrays.fill(dp,-1);
+//        return loot(arr,0,dp);
+//    }
+//
+//    private int loot(int[] arr, int i, int[] dp) {
+//        if(i>=arr.length) return 0;
+//        if(dp[i]!=-1) return dp[i];
+//        int pick = arr[i] + loot(arr,i+2,dp);
+//        int skip = loot(arr,i+1,dp);
+//        return dp[i] = Math.max(pick,skip); // dp[i] stores loot(i)
+//    }
 }
