18aedaa2e7f2a51e17fb4774d7ae7deef81e310a
geekyraghav <49072935+geekyraghav@users.noreply.github.com>
2026-01-17 22:50:28 +0530

    Add files via upload

--- DP/LCS.java
+++ DP/LCS.java
@@ -3,20 +3,32 @@ package DP;
 import java.util.*;
 
 public class LCS {
-    static int lcs(String s1, String s2) { // Tabulation
+    static int lcs(String s1, String s2) { // Tabulation Soch
         int m = s1.length(), n = s2.length();
-        int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int x = (i>=1 && j>=1) ? dp[i-1][j-1] : 0;
-                int y = (i>=1) ? dp[i-1][j] : 0;
-                int z = (j>=1) ? dp[i][j-1] : 0;
-                if(s1.charAt(i)==s2.charAt(j)) dp[i][j] = 1 + x;
-                else dp[i][j] = Math.max(y,z);
+        int[][] dp = new int[m+1][n+1];
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        return dp[m-1][n-1];
+        return dp[m][n];
     }
+
+//    static int lcs(String s1, String s2) { // Tabulation
+//        int m = s1.length(), n = s2.length();
+//        int[][] dp = new int[m][n];
+//        for(int i=0;i<m;i++){
+//            for(int j=0;j<n;j++){
+//                int x = (i>=1 && j>=1) ? dp[i-1][j-1] : 0;
+//                int y = (i>=1) ? dp[i-1][j] : 0;
+//                int z = (j>=1) ? dp[i][j-1] : 0;
+//                if(s1.charAt(i)==s2.charAt(j)) dp[i][j] = 1 + x;
+//                else dp[i][j] = Math.max(y,z);
+//            }
+//        }
+//        return dp[m-1][n-1];
+//    }
 
 //    static int lcs(String s1, String s2) {
 //        int m = s1.length(), n = s2.length();
